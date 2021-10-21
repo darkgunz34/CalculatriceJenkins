5be1bd7bc58a03772495422a56783c0037b3b7d7
stephan <stephan.parichon.pro@gmail.com>
2021-10-21 11:34:29 +0200

    maven

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+N
+src/test/java/LanceurTest.java,d\d\ddca9650f70517fa81b882b681a1ac3e004c168c
+P
+ src/main/java/start/Lanceur.java,4\3\4392e6da0abf565632e7684bb3393daf0c942620
--- src/main/java/start/Lanceur.java
+++ src/main/java/start/Lanceur.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package calculatricejenkins;
+package start;
 
-/**
- *
- * @author steph
- */
-public class CalculatriceJenkins {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Lanceur {
     public static void main(String[] args) {
-        
+
     }
 
     public static int ajouterDeuxNombre(int nombre1, int nombre2) {
@@ -32,5 +20,4 @@ public class CalculatriceJenkins {
     public static int divisionDeuxNombre(int nombre1, int nombre2) {
         return nombre1 / nombre2;
     }
-    
 }
